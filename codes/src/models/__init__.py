a9a66f0b82a867ef3388b8a3421eaafd67ae6a13
mstaczek <matemmatem2000@gmail.com>
2024-04-06 15:20:54 +0200

    Add 2 models - DeiT_T and EfficientNet_B4

--- codes/src/models/__init__.py
+++ codes/src/models/__init__.py
@@ -5,4 +5,6 @@ from .ResNet50 import ResNet50
 from .ViT_B_32 import ViT_B_32
 from .Swin_T import Swin_T
 from .DeiT_S import DeiT_S
+from .DeiT_T import DeiT_T
 from .EfficientNet_B3 import EfficientNet_B3
+from .EfficientNet_B4 import EfficientNet_B4
--- codes/src/strings_to_classes_mappings.py
+++ codes/src/strings_to_classes_mappings.py
@@ -1,5 +1,5 @@
 from src.datasets import Imagenette2Manager
-from src.models import ResNet18, ResNet50, ViT_B_32, Swin_T, DeiT_S, EfficientNet_B3
+from src.models import ResNet18, ResNet50, ViT_B_32, Swin_T, DeiT_S, DeiT_T, EfficientNet_B3, EfficientNet_B4
 from src.explanations import ExplanationGradCAM, ExplanationKernelSHAP
 
 datasets_mapping = {
@@ -8,7 +8,9 @@ datasets_mapping = {
 
 models_mapping = {
     'DeiT_S'          : DeiT_S,
+    'DeiT_T'          : DeiT_T,
     'EfficientNet_B3' : EfficientNet_B3,
+    'EfficientNet_B4' : EfficientNet_B4,
     'Swin_T'          : Swin_T,
     'ResNet18'        : ResNet18,
     'ResNet50'        : ResNet50,
