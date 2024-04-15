eb2c4eff12aade85123a9199ec3d58aa4385b0a4
mstaczek <matemmatem2000@gmail.com>
2024-04-15 19:45:56 +0200

    Reduce number of times model is moved to and from cuda device

--- codes/src/compute_explanations.py
+++ codes/src/compute_explanations.py
@@ -12,13 +12,13 @@ def compute_explanations(dataset_name: str, model_name: str, explanation_name: s
 
     dataloader = dataset_manager.get_dataloader(**kwargs)
 
+    explanation.before_computing_explanations(model)
+
     processed_images = 0
     if number_of_batches_to_process is None:
         number_of_batches_to_process = len(dataloader)
     number_of_batches_to_process = min(number_of_batches_to_process, len(dataloader))
     images_to_process_limit = min(len(dataloader.dataset), number_of_batches_to_process * dataloader.batch_size)
-   
-    model.make_sure_is_initialized()
 
     pbar = tqdm(dataloader, desc='Computing explanations for batches', total=number_of_batches_to_process)
    
@@ -27,8 +27,10 @@ def compute_explanations(dataset_name: str, model_name: str, explanation_name: s
         if processed_images >= images_to_process_limit:
             break
         
-        explanations = explanation.compute_explanation(model, images)
-        predicted_labels = model(images).cpu().detach().numpy().argmax(axis=1).tolist()
+        explanations, predicted_labels = explanation.compute_explanation(model, images)
+        # predicted_labels = model(images).cpu().detach().numpy().argmax(axis=1).tolist()
         dataset_manager.save_explanations(model, explanation, explanations, paths, predicted_labels)
 
         processed_images += len(images)
+
+    explanation.after_computing_explanations(model)
--- codes/src/explanations/Explanation.py
+++ codes/src/explanations/Explanation.py
@@ -7,14 +7,15 @@ class Explanation:
         self.model_name = None
         self.device = device
 
-    def compute_explanation(self, model: Model, images: torch.Tensor) -> torch.Tensor:
+    def before_computing_explanations(self, model: Model):
         model.make_sure_is_initialized()
         model.move_to_device(self.device)
         self._update_model_name(model)
+
+    def compute_explanation(self, model: Model, images: torch.Tensor):
         explanations = self._compute_explanation(model, images)
-        model.move_to_device('cpu')
-        torch.cuda.empty_cache()
-        return explanations
+        predicted_labels = model(images.to(self.device)).cpu().detach().numpy().argmax(axis=1).tolist()
+        return explanations, predicted_labels
     
     def _update_model_name(self, model: Model):
         if self.model_name != model.name:
@@ -24,10 +25,14 @@ class Explanation:
     def _update_explanation_method(self, model: Model):
         raise NotImplementedError
     
-    def _compute_explanation(self, model: Model, images: torch.Tensor) -> torch.Tensor:\
+    def _compute_explanation(self, model: Model, images: torch.Tensor) -> torch.Tensor:
         # internally, handles both gpu and cpu by using self.device. Model is already on self.device
         # return size Batch x H x W
         raise NotImplementedError
     
     def save(self, path_image: str, path_explanation: str, explanation: torch.Tensor):
         raise NotImplementedError
+    
+    def after_computing_explanations(self, model):
+        model.move_to_device('cpu')
+        torch.cuda.empty_cache()
